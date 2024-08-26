448a2e7c555719c35ef75a38aea41b6495060284
Ryunosuke <r.kamata.sys23@morijyobi.ac.jp>
2024-08-26 11:51:02 +0900

    Game.javaの作成 close #6

---
+++ TureOrFalse/src/Game.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Game {
+    // ゲームデータを格納する二次元配列
+    private static String[][] gameData= {{"","",""},{"","",""},{"","",""}};
+
+    //　ゲームデータを配列に格納する関数
+//  格納できた場合はtrueを 格納出来ない場合はfalseを返す
+    public static boolean addGameData(int place,String symbol){
+        return true;
+    }
+
+    //　ゲームの表をコンソールに出力する関数
+    public static void printGameData(){
+
+    }
+
+    //  ゲームの行列をランダムに返す関数()
+//   10の位は行,1の位は列を表す
+    public static int createRandomPlace(){
+        return 0;
+    }
+
+    //  ゲームが勝ち負けを返す関数
+//    勝った場合はtrueをゲームを続行する場合はfalseを返す
+    public static boolean judgement(String symbol){
+        return  true;
+
+    }
+
+
+}
